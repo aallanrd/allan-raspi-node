0d5b05ddcbd4c53d21d79ea28f8a712db643e271
Allan Rojas <allanjose91@gmail.com>
2016-04-11 10:42:16 -0600

    sync

--- index.js
+++ index.js
@@ -9,14 +9,55 @@ var path = require('path');
 var spawn = require('child_process').spawn;
 var proc;
  
-app.use('/', express.static(path.join(__dirname, 'stream')));
+
+var sockets = {};
  
+io.on('connection', function(socket) {
+ 
+  sockets[socket.id] = socket;
+  console.log("Total clientes conectados: ", Object.keys(sockets).length);
+ 
+  socket.on('start-stream', function() {
+         
+    startStreaming(io);
+  });
+
+   socket.on('take', function() {
+         
+    takePicture(io);
+  });
+ 
+});
+ 
+http.listen(3000, function() {
+  console.log('Server  192.168.0.106:3000');
+});
+ 
+
+ 
+function takePicture(io) {
+    
+  	  var args =  ["-r","1280x720","stream/image_stream1.jpg"];
+	  proc = spawn('fswebcam', args);
+ 
+}
+ 
+function startStreaming(io) {
+  	  io.sockets.emit('liveStream', 'stream/image_stream1.jpg');
+ 
+}
+
+
+//Understand to put in other file
+
+app.use('/', express.static(path.join(__dirname, 'stream')));
+
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/stream/image_stream1.jpg', function(req, res) {
-  res.sendFile(__dirname + '/stream/image_stream1.jpg');
+    res.sendFile(__dirname + '/stream/image_stream1.jpg');
 });
 
 app.get('/jquery', function(req, res) {
@@ -34,44 +75,4 @@ app.get('/bootstrapcss', function(req, res) {
 app.get('/fontawesome', function(req, res) {
     res.sendFile(__dirname + '/bower_components/font-awesome/css/font-awesome.css');
 });
-
-var sockets = {};
- 
-io.on('connection', function(socket) {
- 
-  sockets[socket.id] = socket;
-  console.log("Total clients connected : ", Object.keys(sockets).length);
- 
-  socket.on('start-stream', function() {
-         
-    startStreaming(io);
-  });
-
-   socket.on('take', function() {
-         
-    takePicture(io);
-  });
- 
-});
- 
-http.listen(3000, function() {
-  console.log('Server Allan Camara Init *:3000');
-});
- 
-
- 
-function takePicture(io) {
-    
-  	  var args =  ["stream/image_stream1.jpg"];
-	  proc = spawn('fswebcam', args);
- 
-}
- 
-function startStreaming(io) {
- 
-	  
-  	  io.sockets.emit('liveStream', 'stream/image_stream1.jpg');
- 
-}
-
 
