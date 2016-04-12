de0d7dbf288570e757cc85c7b252c533caeb5471
Allan Rojas <allanjose91@gmail.com>
2016-04-11 20:08:50 -0600

    --saveno

--- index.js
+++ index.js
@@ -2,19 +2,13 @@
    Conexión a Raspberry Pi con NodeJS {'express','socket.io'}
  */
 
-
 var express = require('express');
 var app = express();
-
 var sleep = require('sleep');
-
-var io = require('socket.io');
-
 var http = require('http').Server(app);
 
 //See https://nodejs.org/api/fs.html
 var fs = require('fs');
-
 
 var path = require('path');
 
@@ -29,24 +23,22 @@ http.listen(3000, function() {
 
 
 app.get('/takePhoto', function(req, res) {
+
+    takePhoto();
     res.sendFile(__dirname + '/index-started.html');
-    var args =  ["-r","1200x800","stream/image_stream1.jpg"];
-    proc = spawn('fswebcam', args);
+    
 });
 
 app.get('/getPhoto', function(req, res) {
-
-    fs.readFile('stream/image_stream1.jpg', function(err, buf){
-        const bufx = Buffer.from(buf, 'ascii');
-        var imageB64 = bufx.toString('base64');
-        var num = Math.floor((Math.random() * 100000000) + 1);
-        var name = "/tmp/image-"+ num +".txt";
-    });
 
     res.sendfile('stream/image_stream1.jpg')
 
 });
 
+function  takePhoto() {
+    var args =  ["-r","1200x800","stream/image_stream1.jpg"];
+    proc = spawn('fswebcam', args);
+}
 
 //Understand to put in other file
 
